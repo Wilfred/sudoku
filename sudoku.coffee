Wilfred/sudoku

commit 97a60f5b11d6ce6651f1be4939ae4a9097eea5bd

minor refactoring using CS destructuring assignment

sudoku.coffee
@@ -225,10 +225,7 @@ solver =
 
     mostConstrainedPosition = null
 
-    for position in emptyPositions
-      x = position.x
-      y = position.y
-
+    for [x, y] in emptyPositions
       possibilitiesHere = grid.getPossibilities x, y
 
       if possibilitiesHere.length == 0

sudoku.js
@@ -226,13 +226,11 @@
   };
   solver = {
     getMostContstrainedPosition: function(grid) {
-      var emptyPositions, mostConstrainedPosition, position, possibilitiesHere, x, y, _i, _len;
+      var emptyPositions, mostConstrainedPosition, possibilitiesHere, x, y, _i, _len, _ref;
       emptyPositions = grid.getEmptyPositions();
       mostConstrainedPosition = null;
       for (_i = 0, _len = emptyPositions.length; _i < _len; _i++) {
-        position = emptyPositions[_i];
-        x = position.x;
-        y = position.y;
+        _ref = emptyPositions[_i], x = _ref[0], y = _ref[1];
         possibilitiesHere = grid.getPossibilities(x, y);
         if (possibilitiesHere.length === 0) {
           return {

commit 348c18cdbf824f89fc4e4b5036e27679697317ea

fixing destructuring assignment broken in 97a60f5 because getEmptyPositions returns an array of objects

sudoku.coffee
@@ -225,7 +225,7 @@ solver =
 
     mostConstrainedPosition = null
 
-    for [x, y] in emptyPositions
+    for {x: x, y: y} in emptyPositions
       possibilitiesHere = grid.getPossibilities x, y
 
       if possibilitiesHere.length == 0